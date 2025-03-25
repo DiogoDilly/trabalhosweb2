81cb7b9d70525dbf08c1fabab67f13e975b29689
xLorenzoSCx <lorenzoclarindo456@gmail.com>
2025-03-25 15:26:38 -0300

    coisandocoisascoisadosdocoisa

--- trabalhoContext/src/app.jsx
+++ trabalhoContext/src/app.jsx
@@ -1,9 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { Home } from "./pages/home";
+import {api} from "./services/api"
+
 
 import { ThemeProvider } from "./context/ThemeProvider";
+import { useEffect, useState } from "react";
 
 export function App() {
+const [profileImg, setProfileImg] = useState(null);
+const [userName, setUserName] = UserState(null);
+
+
+
   return (
     <ThemeProvider>
       <Router>
